81df59b71358b1251d95a33a7722c2284f582178
adamaclp92 <adamaclp92@gmail.com>
2021-04-27 21:02:39 +0200

    Login

--- src/progtech_beadando/Login.java
+++ src/progtech_beadando/Login.java
@@ -7,15 +7,29 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.awt.event.ActionEvent;
 
 public class Login extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textUsername;
 	private JPasswordField passwordField;
+	
+	private static final Connection con = new DB().getCon();
+	private ResultSet rs;
+	private Statement stm;
 
 	public Login() {
 		setVisible(true);
@@ -48,7 +62,39 @@ public class Login extends JFrame {
 		passwordField.setBounds(24, 191, 96, 19);
 		contentPane.add(passwordField);
 		
+		JButton btnLogin = new JButton("Bejelentkez\u00E9s");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				try {
+					char[] pw = passwordField.getPassword();
+					String pwConcat = "";
+					for(char c : pw) {
+						pwConcat += c;
+					}
+					
+					stm = con.createStatement();
+					rs = stm.executeQuery("SELECT username FROM users WHERE username=" + "'" + textUsername.getText() + "' AND password=" + "'" + pwConcat + "'");
+
+					if(rs.next()) {
+						new Inic();
+						dispose();
+					}else {
+						JOptionPane.showMessageDialog(null, "Hibás felhasználónév vagy jelszó!");
+					}
+					
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+
+			}
+		});
+		btnLogin.setBounds(133, 152, 128, 29);
+		contentPane.add(btnLogin);
+		
 		repaint();
 		validate();
 	}
+	
 }
